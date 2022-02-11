5f142b477cd84205f2cf0345ec1334214001a1e1
Aviral <aviral1101@gmail.com>
2022-02-12 01:20:31 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+import Login from "./Pages/login/signup";
+import axios from "axios";
 export default function () {
-  return <div>Hello Motto!</div>;
+  const handleClick = async () => {
+    await axios
+      .get("http://localhost:5000/")
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>click me</button>
+      <Login />
+    </div>
+  );
 }
